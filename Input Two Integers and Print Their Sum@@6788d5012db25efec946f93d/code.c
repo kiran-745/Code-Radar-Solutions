1209a0249743aa761a734db7d30e90ea08a1cacd
KIRAN <kirankalirawana614@gmail.com>
2025-02-13 13:32:17 +0530

    Solution to Input Two Integers and Print Their Sum

---
+++ Input Two Integers and Print Their Sum@@6788d5012db25efec946f93d/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+    scanf("%d %d", &num1, &num2); // Read two integers separated by a space
+
+    sum = num1 + num2; // Calculate the sum
+
+    printf("%d\n", sum); // Print the sum followed by a newline
+
+    return 0;
+}
